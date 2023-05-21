4d7fbe15d28ae1521f6498a931f4c97374acb536
Amin Bahiraei <amin.bahiraei.75@gmail.com>
2023-05-21 09:49:35 +0330

    change result

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
-import express, { Express, Request, Response } from 'express';
+import express, {Express, Request, Response} from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
+const port: number = Number(process.env.PORT || 8080);
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
+    res.send('Hello World');
 });
 
 app.listen(port, () => {
